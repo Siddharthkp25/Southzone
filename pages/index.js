cba77b0a85d5dcb878f8cd39b9b4268ac81a347e
Siddhukp68 <30855461+Siddharthkp25@users.noreply.github.com>
2024-10-19 14:46:20 +0530

    update

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 // MODULES //
-
-// COMPONENTS //
+import { useState, useEffect } from "react";
 import Head from "next/head";
 import Footer from "../src/components/Footer";
 import Header from "../src/components/Header";
@@ -16,33 +15,54 @@ import Clients from "@/sections/home/Clients";
 
 // PLUGINS //
 import { gsap } from "gsap";
+
 // STYLES //
 import styles from "../src/styles/pages/Home.module.scss";
 
-
-
-
 /** Home Page */
 export default function Home() {
-	return (
-		<div> 
-			<Head>
-				<title>Home</title>
-				<meta name="description" content="Southzone construction & interiors also specializes in space analysis, planning, designing & construction and facility management." />
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
+  // State to track if the intro has been viewed
+  const [isIntroViewed, setIntroViewed] = useState(false);
 
-			<Header />
-			<main className={`${styles.index_page} bg_secondary`}>
-				<HomeIntro gsap={gsap} />
-				<HomeHero />
-				<Service />
-				<Founder />
-				<OurProjects />
-				<Portfolio />
-				<Clients />
-			</main>
-			<Footer />
-		</div>
-	);
+  // Check localStorage on component mount
+  useEffect(() => {
+    const introViewedInLocal = localStorage.getItem("introViewed");
+
+    // If intro has been viewed, skip showing it
+    if (introViewedInLocal === "true") {
+      setIntroViewed(true); // Intro has already been viewed
+    }
+  }, []);
+
+  // Function to handle when the intro animation is finished
+  const handleIntroEnd = () => {
+    localStorage.setItem("introViewed", "true"); // Mark intro as viewed in localStorage
+    setIntroViewed(true); // Update state to hide the intro
+  };
+
+  return (
+    <div>
+      <Head>
+        <title>Home</title>
+        <meta
+          name="description"
+          content="Southzone construction & interiors also specializes in space analysis, planning, designing & construction and facility management."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+      <main className={`${styles.index_page} bg_secondary`}>
+        {/* Only show HomeIntro if it has not been viewed */}
+        {!isIntroViewed && <HomeIntro gsap={gsap} onEnd={handleIntroEnd} />}
+        <HomeHero />
+        <Service />
+        <Founder />
+        <OurProjects />
+        <Portfolio />
+        <Clients />
+      </main>
+      <Footer />
+    </div>
+  );
 }
--- src/sections/home/HomeIntro.js
+++ src/sections/home/HomeIntro.js
@@ -1,14 +1,6 @@
 // MODULES //
-import { useEffect, useRef, useState } from "react";
-
-// COMPONENTS //
+import { useEffect, useRef } from "react";
 import Image from "next/image";
-
-// SECTIONS //
-
-// PLUGINS //
-
-// UTILS //
 
 // STYLES //
 import styles from "@/styles/sections/home/HomeIntro.module.scss";
@@ -16,110 +8,116 @@ import styles from "@/styles/sections/home/HomeIntro.module.scss";
 // IMAGES //
 import Logo from "@/../public/img/logo.jpg";
 
-// DATA //
-
 /** HomeIntro Section */
-export default function HomeIntro({ gsap }) {
-	const sectionRef = useRef();
+export default function HomeIntro({ gsap, onEnd }) {
+  const sectionRef = useRef();
 
-	const isU = gsap.utils.selector(sectionRef);
+  const isU = gsap.utils.selector(sectionRef);
 
-	/** Intro Animation */
-	function introAnimation() {
-		const html = document.querySelector("html");
+  /** Intro Animation */
+  function introAnimation() {
+    const html = document.querySelector("html");
 
-		// Start with the HTML element having the "overflowHidden" class
-		html.classList.add("overflowHidden");
+    // Start with the HTML element having the "overflowHidden" class
+    html.classList.add("overflowHidden");
 
-		const introTween = gsap.timeline({
-			onComplete: function () {
-				html.classList.remove("overflowHidden");
-			},
-		});
+    const introTween = gsap.timeline({
+      onComplete: function () {
+        html.classList.remove("overflowHidden");
 
-		introTween
-			.to(isU(".scene_1"), {
-				delay: 1,
-			})
-			.to(
-				isU(".scene_1 .logo"),
-				{
-					duration: 1.5,
-					opacity: 0,
-					delay: 1,
-				},
-				"first"
-			)
-			.to(
-				isU(".scene_1"),
-				{
-					duration: 1.5,
-					y: "-100%",
-					delay: 1,
-				},
-				"second"
-			)
-			.to(
-				isU(`.${styles.scene_2}`),
-				{
-					duration: 1,
-					y: "-100%",
-				},
-				"second"
-			)
-			.to(
-				isU(`.${styles.scene_2} .${styles.intro_content} .${styles.title}`),
-				{
-					duration: 1.2,
-					y: "0%",
-				},
-				"second"
-			)
-			.to(
-				isU(".scene_2"),
-				{
-					duration: 1,
-					y: "-200%",
-				},
-				"third"
-			)
-			.to(
-				isU(`.${styles.scene_2} .${styles.intro_content}`),
-				{
-					duration: 0.8,
-					y: "250%",
-				},
-				"third"
-			)
-			.to(
-				".home_intro",
-				{
-					display: "none",
-				},
-				"fourth"
-			);
-	}
+        // Call onEnd when animation finishes to trigger localStorage set
+        onEnd();
+      },
+    });
 
-	useEffect(() => {
-		introAnimation();
-	}, []);
+    introTween
+      .to(isU(".scene_1"), {
+        delay: 1,
+      })
+      .to(
+        isU(".scene_1 .logo"),
+        {
+          duration: 1.5,
+          opacity: 0,
+          delay: 1,
+        },
+        "first"
+      )
+      .to(
+        isU(".scene_1"),
+        {
+          duration: 1.5,
+          y: "-100%",
+          delay: 1,
+        },
+        "second"
+      )
+      .to(
+        isU(`.${styles.scene_2}`),
+        {
+          duration: 1,
+          y: "-100%",
+        },
+        "second"
+      )
+      .to(
+        isU(`.${styles.scene_2} .${styles.intro_content} .${styles.title}`),
+        {
+          duration: 1.2,
+          y: "0%",
+        },
+        "second"
+      )
+      .to(
+        isU(".scene_2"),
+        {
+          duration: 1,
+          y: "-200%",
+        },
+        "third"
+      )
+      .to(
+        isU(`.${styles.scene_2} .${styles.intro_content}`),
+        {
+          duration: 0.8,
+          y: "250%",
+        },
+        "third"
+      )
+      .to(
+        isU(".home_intro"),
+        {
+          duration: 0.5,
+          autoAlpha: 0, // Fade out and hide
+          onComplete: () => {
+            gsap.set(isU(".home_intro"), { display: "none" }); // Apply display: none
+          },
+        },
+        "fourth"
+      );
+  }
 
-	return (
-		<section className={`${styles.HomeIntro} home_intro`} ref={sectionRef}>
-			<div className={`${styles.intro_wrapper}`}>
-				<div className={`${styles.scene_1} scene_1 bg_white`}>
-					<div className={`${styles.logo} logo next_img`}>
-						<Image src={Logo} alt="" />
-					</div>
-				</div>
-				<div className={`${styles.scene_2} scene_2 bg_primary text_center`}>
-					<div className={`${styles.intro_content}`}>
-						<h2 className={`${styles.title} title text_xxl text_700`}>
-						"Good design is about making spaces that not <br/> only look beautiful but also feel like home."
-						</h2>
-					</div>
-				</div>
-			</div>
-		</section>
-	);
+  useEffect(() => {
+    introAnimation();
+  }, []);
+
+  return (
+    <section className={`${styles.HomeIntro} home_intro`} ref={sectionRef}>
+      <div className={`${styles.intro_wrapper}`}>
+        <div className={`${styles.scene_1} scene_1 bg_white`}>
+          <div className={`${styles.logo} logo next_img`}>
+            <Image src={Logo} alt="Company Logo" />
+          </div>
+        </div>
+        <div className={`${styles.scene_2} scene_2 bg_primary text_center`}>
+          <div className={`${styles.intro_content}`}>
+            <h2 className={`${styles.title} title text_xxl text_700`}>
+              "Good design is about making spaces that not <br /> only look
+              beautiful but also feel like home."
+            </h2>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
